b21c51db059f0db60d480566e9dff5d009d38175
amiller5233@gmail.com <amiller5233@gmail.com>
2023-07-20 22:10:57 -0700

    create xcode project

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Adam Miller on 7/20/23.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
